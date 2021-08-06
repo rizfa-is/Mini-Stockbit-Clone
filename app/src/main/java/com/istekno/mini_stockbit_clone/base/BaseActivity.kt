2c21ac59d91566854e051708e730ea0f67554eec
Rosyid <rosyidrosadi15@gmail.com>
2021-08-07 01:58:04 +0700

    Add dark mode state

--- app/src/main/java/com/istekno/mini_stockbit_clone/base/BaseActivity.kt
+++ app/src/main/java/com/istekno/mini_stockbit_clone/base/BaseActivity.kt
@@ -1,20 +1,35 @@
 package com.istekno.mini_stockbit_clone.base
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.istekno.mini_stockbit_clone.utils.DarkModeState
 
 abstract class BaseActivity<binding: ViewDataBinding>: AppCompatActivity() {
 
     abstract val layout: Int
+    open val darkModeState: DarkModeState? = null
     lateinit var dataBinding: binding
 
     abstract fun setupLifeCycleOwner()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setupDarkModeState()
         super.onCreate(savedInstanceState)
         dataBinding= DataBindingUtil.setContentView(this, layout)
         setupLifeCycleOwner()
+    }
+
+    private fun setupDarkModeState() {
+        if (darkModeState != null) {
+            val detectMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            when(detectMode) {
+                Configuration.UI_MODE_NIGHT_YES -> darkModeState?.onDarkMode()
+                Configuration.UI_MODE_NIGHT_NO -> darkModeState?.onLightMode()
+                Configuration.UI_MODE_NIGHT_UNDEFINED -> darkModeState?.onLightMode()
+            }
+        }
     }
 }
--- app/src/main/java/com/istekno/mini_stockbit_clone/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/istekno/mini_stockbit_clone/ui/onboarding/OnboardingActivity.kt
@@ -1,12 +1,41 @@
 package com.istekno.mini_stockbit_clone.ui.onboarding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.istekno.mini_stockbit_clone.R
+import com.istekno.mini_stockbit_clone.base.BaseActivity
+import com.istekno.mini_stockbit_clone.databinding.ActivityOnboardingBinding
+import com.istekno.mini_stockbit_clone.utils.DarkModeState
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>(), DarkModeState {
+
+    override val layout: Int
+        get() = R.layout.activity_onboarding
+
+    override fun setupLifeCycleOwner() {
+        dataBinding.apply {
+            lifecycleOwner = this@OnboardingActivity
+        }
+    }
+
+    override val darkModeState: DarkModeState?
+        get() = this
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        dataBinding.tvMyText.text = "This is Stockbit"
+    }
+
+    override fun onLightMode() {
+        Toast.makeText(this, "This is Light Mode", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDarkMode() {
+        Toast.makeText(this, "This is Dark Mode", Toast.LENGTH_LONG).show()
     }
 }
